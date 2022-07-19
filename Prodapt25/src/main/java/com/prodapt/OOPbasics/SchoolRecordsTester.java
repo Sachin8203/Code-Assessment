43a54ce8da233094879de667126caeb8efa6cdb9
sachidhanantha.d <sachidhanantha.d@CLAP2958.prodapt.com>
2022-07-19 09:48:42 +0530

    week1 assessment

---
+++ Prodapt25/src/main/java/com/prodapt/OOPbasics/SchoolRecordsTester.java
@@ -0,0 +1,15 @@
+package com.prodapt.OOPbasics;
+
+public class SchoolRecordsTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SchoolRecords ob1 = new SchoolRecords();
+		ob1.displayRecords();
+		ob1.addRecords(111, "SunBeam Matriculation", "Neelankarai" );
+		ob1.addRecords(222, "British International", "Neelankarai" );
+		ob1.addRecords(333, "St.John Universal", "Pallvakam" );
+
+	}
+
+}
---
+++ Prodapt25/src/main/java/com/prodapt/OOPbasics/SiTester.java
@@ -0,0 +1,14 @@
+package com.prodapt.OOPbasics;
+
+public class SiTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SI.principle = 5000;
+		SI.rate = 10;
+		SI.time = 2;
+		SI.simpleInterest();
+
+	}
+
+}
