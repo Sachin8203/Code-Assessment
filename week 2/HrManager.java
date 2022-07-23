3a980cb93ae2f863ecf4499294aa4ce97c6cdfd0
Sachin8203 <108717914+Sachin8203@users.noreply.github.com>
2022-07-23 21:50:42 +0530

    Create HrManager.java

---
+++ week 2/HrManager.java
@@ -0,0 +1,10 @@
+package assessment;
+
+public class HrManager {
+	static void processRecords(String name,int age,String designation) {
+		System.out.print("\nEmployee name : "+name);
+		System.out.print(", Employee age : "+age);
+		System.out.print(", Employee designation : "+designation);
+	}
+
+}
