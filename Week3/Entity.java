854c63f8a2aae3c48ed0426fb96abc1f2b0637c9
Sachin8203 <108717914+Sachin8203@users.noreply.github.com>
2022-08-02 20:35:14 +0530

    Create Entity.java

---
+++ Week3/Entity.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class Entity {
+	int numberOfBooks=3;
+	int n=numberOfBooks-1;
+	int id[]= {1001,2001,3001};
+	String name[]={"Harry potter","Half grilfrind","Alice in Wonderland"};
+	String author[]= {"J.K.Rowling","Chetan Bhagat","Lewis Carroll"};
+
+}
